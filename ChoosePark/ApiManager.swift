efe1755c82773ada37b96389b25a1856b9653d2e
ethan1022 <ethan1022@gmail.com>
2018-03-24 15:12:09 +0800

    add apiManager and parkScene class

---
+++ ChoosePark/ApiManager.swift
@@ -0,0 +1,30 @@
+//
+//  ApiManager.swift
+//  ChoosePark
+//
+//  Created by Ethan on 2018/3/24.
+//  Copyright © 2018年 EthanLab. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ApiManager : NSObject {
+    
+    let requestUrl = URL(string:"http://data.taipei/opendata/datalist/apiAccess")
+    
+    func fetchNewDataWithLimitNumber(_ limit:Int? , offset:Int?) {
+        var paramDic : Dictionary = ["scope":"resourceAquire", "rid":"bf073841-c734-49bf-a97f-3757a6013812"]
+        if let limit = limit {
+            paramDic["limit"] = "\(limit)"
+        }
+        if let offset = offset {
+            paramDic["offset"] = "\(offset)"
+        }
+        
+        Alamofire.request(requestUrl!, method: .get, parameters: paramDic, encoding: URLEncoding.default, headers: nil).responseJSON { (dataResponse) in
+            print(dataResponse)
+        }
+    }
+    
+}
---
+++ ChoosePark/ParkScene.swift
@@ -0,0 +1,13 @@
+//
+//  ParkScene.swift
+//  ChoosePark
+//
+//  Created by Ethan on 2018/3/24.
+//  Copyright © 2018年 EthanLab. All rights reserved.
+//
+
+import Foundation
+
+class ParkScene: NSObject {
+    
+}
